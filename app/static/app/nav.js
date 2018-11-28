eed532c26e172cdfdd76eab0bf5f0cbda3bfcdeb
gingerkeys <gkeys@basecampcodingacademy.org>
2018-11-28 14:19:07 -0600

    changed the jQuery to vanilla JavaScript.

--- app/static/app/nav.js
+++ app/static/app/nav.js
@@ -1,7 +1,15 @@
 function main() {
-    $('#nav-view-submit').click(() => {
-        location = '/view/' + encodeURI($('#nav-view-short-code-input').val());
-    });
+    // $('#nav-view-submit').click(() => {
+    //     location = '/link/' + encodeURI($('#nav-view-short-code-input').val());
+    // });
+
+    var button = document.getElementById("nav-view-submit")
+    button.addEventListener("click", function () {
+        var input = document.getElementById("nav-view-short-code-input").value
+
+        location = '/link/' + encodeURI(input)
+    })
 }
 
-$(main);
+// $(main);
+main()
